d3eb0e7ec1dd01474133ebf14ac410be3237b220
GardenandCompany <till.schmidt-ott@ehl.ch>
2025-05-04 15:38:47 +0200

    Delete components/ui/card.js

--- components/ui/card.js
+++
@@ -1,7 +0,0 @@
-
-export function Card({ children, ...props }) {
-  return <div {...props} className='rounded-2xl shadow bg-white'>{children}</div>;
-}
-export function CardContent({ children, ...props }) {
-  return <div {...props}>{children}</div>;
-}
