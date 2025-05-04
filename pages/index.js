b58dc505270c8cf819ccbe664e689c98c90d0167
GardenandCompany <till.schmidt-ott@ehl.ch>
2025-05-04 15:00:43 +0200

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+      <h1>Garden&Company</h1>
+      <p>Welcome to our store – we're getting ready to launch!</p>
+    </div>
+  );
+}
